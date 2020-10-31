526578b790fbf8883d2638cecc34595a5fe9688b
takumin0423 <takumin0423baske@gmail.com>
2020-11-01 01:39:52 +0900

    SignUpコンポーネントの作成

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Switch, Route} from 'react-router';
-import {Home, Login} from './pages';
+import {Home, Login, SignUp} from './pages';
 
 const Router = () => {
   return (
       <Switch>
         <Route exact path={'(/)?'} component={Home}/>
+        <Route exact path={'/signup'} component={SignUp}/>
         <Route exact path={'/login'} component={Login}/>
       </Switch>
   );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {TextInput} from '../components/generic';
 
 const SignUp = () => {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const inputUserName = useCallback((event) => {
+    setUserName(event.target.value);
+  }, [setUserName]);
+
+  const inputEmail = useCallback((event) => {
+    setEmail(event.target.value);
+  }, [setEmail]);
+
+  const inputPassword = useCallback((event) => {
+    setPassword(event.target.value);
+  }, [setPassword]);
+
+  const inputConfirmPassword = useCallback((event) => {
+    setConfirmPassword(event.target.value);
+  }, [setConfirmPassword]);
+
   return (
       <div className="container">
         <h2 className="text-headline text-center">アカウント登録</h2>
@@ -9,7 +30,49 @@ const SignUp = () => {
         {/* 空白を表現するためのDOM */}
         <div className="medium-space"/>
 
-        <TextInput/>
+        <TextInput
+            fullWidth={true}
+            label={'ユーザー名'}
+            multiline={false}
+            required={true}
+            rows={1}
+            value={userName}
+            type={'text'}
+            onChange={inputUserName}
+        />
+
+        <TextInput
+            fullWidth={true}
+            label={'メールアドレス'}
+            multiline={false}
+            required={true}
+            rows={1}
+            value={email}
+            type={'email'}
+            onChange={inputEmail}
+        />
+
+        <TextInput
+            fullWidth={true}
+            label={'パスワード'}
+            multiline={false}
+            required={true}
+            rows={1}
+            value={password}
+            type={'password'}
+            onChange={inputPassword}
+        />
+
+        <TextInput
+            fullWidth={true}
+            label={'パスワード(再確認)'}
+            multiline={false}
+            required={true}
+            rows={1}
+            value={confirmPassword}
+            type={'password'}
+            onChange={inputConfirmPassword}
+        />
       </div>
   );
 };
