51cba9df8c03bc51baa8935f9da11fd85df6008c
takumin0423 <takumin0423baske@gmail.com>
2020-10-31 15:42:42 +0900

    historyのバージョン変更、遷移時エラーが発生しないことを確認

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Switch, Route} from 'react-router';
+import {Home, Login} from './pages';
 
 const Router = () => {
   return (
       <Switch>
-        <Route exact path={'/login'}/>
-        <Route exact path={'(/)?'}/>
+        <Route exact path={'(/)?'} component={Home}/>
+        <Route exact path={'/login'} component={Login}/>
       </Switch>
   );
 };
