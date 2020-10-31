34e020fcdc6c382fa435e79ee038411a5181c27b
takumin0423 <takumin0423baske@gmail.com>
2020-10-31 18:28:19 +0900

    firebaseの初期設定、auth設定

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCXePp3aybI7f5dR_tB5BO6Xhwo5zmeD_I',
+  authDomain: 'react-ec-app-53d62.firebaseapp.com',
+  databaseURL: 'https://react-ec-app-53d62.firebaseio.com',
+  projectId: 'react-ec-app-53d62',
+  storageBucket: 'react-ec-app-53d62.appspot.com',
+  messagingSenderId: '883915086143',
+  appId: '1:883915086143:web:17df2404fb8c3ed6015b8a',
+  measurementId: 'G-RQ5MSJNRLS',
+};
---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+import 'firebase/functions';
+import {firebaseConfig} from './config';
+
+firebase.initializeApp(firebaseConfig);
+
+// 各firebaseサービスのメソッドを定数化
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+export const functions = firebase.functions();
+export const firebaseTimestamp = firebase.firestore.Timestamp;
