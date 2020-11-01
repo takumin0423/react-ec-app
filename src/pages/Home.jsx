2bdc0a3d24149b62f87a98df734705e8a3a35f3a
takumin0423 <takumin0423baske@gmail.com>
2020-11-02 00:29:19 +0900

    ログアウト機能を実装

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {getUserId, getUserName} from '../reducks/users/selectors';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {signOut} from '../reducks/users/operations';
 
 const Home = () => {
+  const dispatch = useDispatch();
   const selector = useSelector(state => state);
   const uid = getUserId(selector);
   const userName = getUserName(selector);
@@ -12,6 +14,9 @@ const Home = () => {
         <h2>Home</h2>
         <p>{uid}</p>
         <p>{userName}</p>
+        <button onClick={() => dispatch(signOut())}>
+          ログアウト
+        </button>
       </div>
   );
 };
