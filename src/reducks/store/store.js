7b47fc669af7c755aa88531cf4bd8bcf7d2a84a3
takumin0423 <takumin0423baske@gmail.com>
2020-11-02 20:40:04 +0900

    initialStateにproductsを追加

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -2,12 +2,14 @@ import {createStore as reduxCreateStore, combineReducers, applyMiddleware} from 
 import {UsersReducer} from '../users/reducers';
 import {connectRouter, routerMiddleware} from 'connected-react-router';
 import thunk from 'redux-thunk';
+import {ProductsReducer} from '../products/reducers';
 
 export default function createStore(history) {
   return reduxCreateStore(
       combineReducers({
         router: connectRouter(history),
         users: UsersReducer,
+        products: ProductsReducer,
       }),
       applyMiddleware(
           routerMiddleware(history),
