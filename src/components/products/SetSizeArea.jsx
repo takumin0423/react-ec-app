437d44539fb04452683753fe533e153805dbb389
takumin0423 <takumin0423baske@gmail.com>
2020-11-03 01:53:59 +0900

    必要なmaterialUIのライブラリをインポート

--- src/components/products/SetSizeArea.jsx
+++ src/components/products/SetSizeArea.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import {TableContainer} from '@material-ui/core/TableContainer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import IconButton from '@material-ui/core/IconButton';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 const SetSizeArea = (prosp) => {
+  return (
+      <div>
+        <TableContainer component={Paper}>
 
+        </TableContainer>
+      </div>
+  );
 };
 
 export default SetSizeArea;
