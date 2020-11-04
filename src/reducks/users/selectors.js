333c0cfaeab65533b38277eedae3bf3e15047fcc
takumin0423 <takumin0423baske@gmail.com>
2020-11-05 01:53:55 +0900

    usersselectorにカートの中身を取得するselectorを作成

--- src/reducks/users/selectors.js
+++ src/reducks/users/selectors.js
@@ -16,3 +16,8 @@ export const getIsSignedIn = createSelector(
     [usersSelector],
     state => state.isSignedIn,
 );
+
+export const getProductsInCart = createSelector(
+    [usersSelector],
+    state => state.cart,
+);
