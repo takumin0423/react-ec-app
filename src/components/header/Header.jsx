ccd9eb67310afce8a80b5d80b47d395c8cab22da
takumin0423 <takumin0423baske@gmail.com>
2020-11-04 10:26:04 +0900

    Headerにサイトタイトルを追加

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import {useDispatch, useSelector} from 'react-redux';
+import {getIsSignedIn} from '../../reducks/users/selectors';
+import {push} from 'connected-react-router';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuBar: {
+    backgroundColor: '#F9CBA4',
+    color: '#fff',
+  },
+  toolbar: {
+    margin: '0 auto',
+    maxWidth: 1024,
+    width: '100%',
+  },
+  iconButtons: {
+    margin: '0 0 0 auto',
+  },
+});
 
 const Header = (props) => {
-  return (
+  const classes = useStyles();
+  const selector = useSelector(state => state);
+  const isSignedId = getIsSignedIn(selector);
 
+  return (
+      <div className={classes.root}>
+        <AppBar
+            position={'fixed'}
+            className={classes.menuBar}
+        >
+          <Toolbar className={classes.toolbar}>
+            <div>
+              <span className="header-text">ANIMAL PHOTOGRAPH</span>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
   );
 };
 
